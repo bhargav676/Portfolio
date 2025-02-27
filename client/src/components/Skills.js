59b26d91881b11313ebacbc0b8624f2df21ab81f
Bhargav <bhargavk1290@gmail.com>
2025-02-27 18:10:58 +0530

    about3

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -4,17 +4,6 @@ const Skills = () => {
     <div>
       <p>html</p>
       <p>I am a Full stack Developer in Mern stack</p>
-                        <p>
-                            Welcome to my portfolio! I hope you enjoy your visit. I specialize in
-                            Back-End development with Node.js and Front-End development with React.js.
-                            I have worked on various projects, including a Real Link-Shortener,
-                            E-Commerce APIs, React-based Website Design and Development, and more.
-                        </p>
-                        <p>
-                            I am passionate about learning new technologies and building solutions that
-                            make a difference. Thank you for visiting my portfolio! I hope you like my
-                            work. Happy coding!
-                        </p>
     </div>
   )
 }
